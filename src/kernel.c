4e631082f873fe2fa4eacd47d502ba97acee75c8
AlphaGotReal <patrasubham2004@gmail.com>
2024-07-28 16:16:53 +0530

    printing colored string

--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,21 @@
+
+void print(char *);
+
+void main() {
+  char msg[] = "        \0";
+  print(&msg[0]);
+}
+
 void print(char *string) {
-  char r;
+  int r;
   int t;
   while (1) {
     r = string[t];
     if (r == 0) break;
     *(char *)(0xb8000 + t*2) = r;
+    *(char *)(0xb8000 + t*2 + 1) = 0x6f;
     t++;
   }
 }
 
-void main() {
-  char msg[] = "Hello world\0";
-  print(&msg[0]);
-}
 
